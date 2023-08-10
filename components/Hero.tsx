eccb47d24c093572fe4244416a2c911802b27416
rizerphe <44440399+rizerphe@users.noreply.github.com>
2023-08-10 19:01:13 +0300

    Lower heading font on mobile

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,9 @@
 export default function Hero() {
   return (
     <div className="text-center flex flex-col items-center gap-4">
-      <h1 className="text-4xl font-bold">Clyde&apos;s Full of Surprises</h1>
+      <h1 className="text-2xl md:text-4xl font-bold">
+        Clyde&apos;s Full of Surprises
+      </h1>
 
       <p className="text-lg">
         Enhance your Discord chats with Clyde&apos;s secret notes
